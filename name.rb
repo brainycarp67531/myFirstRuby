eaa164dcb81f8ee52209e143c2ab39f214a167f8
brainycarp67531 <108860913+brainycarp67531@users.noreply.github.com>
2025-09-22 12:21:19 +0200

    Add last name prompt and repeated greeting
    The script now asks for the user's last name, greets them using both first and last names, and prints the full name 10 times.

--- name.rb
+++ name.rb
@@ -1,4 +1,12 @@
 puts "What is your name?\n"
 first_name = gets.chomp
 
-puts "Hello " + first_name + " and welcome!"
+# Ask for the last name and store it in a variable
+puts "What is you last name?\n"
+last_name = gets.chomp
+
+puts "Hello " + first_name + " " +  last_name + " and welcome!"
+
+10.times do |n|
+  puts first_name + " " + last_name
+end
